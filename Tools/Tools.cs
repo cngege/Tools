bc034dbd9cd8e6adc11975a4b589d5920d2ffd5c
muzili <2586850402@qq.com>
2020-03-12 13:59:26 +0800

    添加项目文件。

---
+++ Tools/Tools.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Tools
+{
+    public class Error 
+    {
+        public String Type;
+        public String Message;
+
+        public Error(String _type) { }
+    }
+
+    namespace net
+    {
+        public class Get
+        {
+            /// <summary>
+            /// 发生网络Get请求
+            /// </summary>
+            /// <param name="getUrl">请求的URL</param>
+            /// <param name="contentType">链接样式 [可空]默认"text/json"</param>
+            public Get(string getUrl, string contentType = "text/json")
+            { 
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public class Download
+        {
+
+        }
+    }
+
+}
