15b5a605638680ac0de961a16b64aaac6d3e36fb
muzili <2586850402@qq.com>
2020-04-15 16:10:45 +0800

    小更新、改动

---
+++ Tools/Data.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    //数据处理
+    namespace Data
+    {
+        public class JSON : JObject
+        {
+            /// <summary>
+            /// 将JSON格式的字符串重新反序列化
+            /// </summary>
+            /// <param name="_json">JSON格式的字符串</param>
+            /// <returns></returns>
+            public static JSON ToJson(String _json)
+            {
+                return (JSON)Parse(_json);
+            }
+        }
+
+
+    }
+}
--- Tools/Tools.cs
+++ Tools/Tools.cs
@@ -1,9 +1,7 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Threading;
+
 
 namespace Tools
 {
@@ -38,24 +36,7 @@ namespace Tools
     }
 
 
-    //数据处理
-    namespace Data
-    {
-        public class JSON : JObject 
-        {
-            /// <summary>
-            /// 将JSON格式的字符串重新反序列化
-            /// </summary>
-            /// <param name="_json">JSON格式的字符串</param>
-            /// <returns></returns>
-            public static JSON ToJson(String _json)
-            {
-                return (JSON)Parse(_json);
-            }
-        }
 
-
-    }
 
     //"简单化线程"
     namespace SimpleThread
