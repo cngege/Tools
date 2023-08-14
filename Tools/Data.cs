e89a7609aef46496f9848e4e2215994fd850474a
muzili <2586850402@qq.com>
2023-08-15 00:51:01 +0800

    概率部分完成

--- Tools/Data.cs
+++ Tools/Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
@@ -61,6 +62,57 @@ namespace Tools
             public static void CopyData(string format,object data)
             {
                 Clipboard.SetData(format, data);
+            }
+        }
+
+        /// <summary>
+        /// 以给定的概率返回 <br>
+        /// 比如 1 2 3 4 分别以 10%,20%,30%,40%的概率获取其中的一个
+        /// </summary>
+        public class Randoms
+        {
+            Random random;
+
+            public Randoms()
+            {
+                random = new Random();
+            }
+
+            public Randoms(int seed)
+            {
+                random = new Random(seed);
+            }
+
+            /// <summary>
+            /// 传入一个数组
+            /// </summary>
+            /// <param name="probability">每个成员都是一个概率0-100之间,和等于100</param>
+            /// <returns>返回选中这个概率的下标</returns>
+            /// <exception cref="ArgumentException">概率没有按照要求传入</exception>
+            public int Ranges(int[] probability)
+            {
+                // 数组成员值必须在 0 - 100 之间, 且 和必须为100
+                int[] cumulativeData = new int[probability.Length];
+                for (int i = 0; i < probability.Length; i++)
+                {
+                    if (probability[i] < 0 || probability[i] > 100) throw new ArgumentException("参数成员值范围不在0-100之间", "probability[" + i.ToString() + "]");
+                    cumulativeData[i] = (i == 0) ? probability[i] : cumulativeData[i - 1] + probability[i];
+                }
+                if (cumulativeData[cumulativeData.Length - 1] != 100) throw new ArgumentException("参数成员值的和不为100", "probability");
+
+                int v = random.Next(100);
+                for (int i = 0; i < cumulativeData.Length; i++)
+                {
+                    if (v < cumulativeData[i])
+                    {
+                        if (i == 0 || v >= cumulativeData[i - 1])
+                        {
+                            return i;
+                        }
+                    }
+                }
+                // 理论永远不会触发的返回值
+                return 0;
             }
         }
 
