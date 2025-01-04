6c395cb1f1cafa155f6746c08740a74597d7e469
muzili <2586850402@qq.com>
2025-01-05 04:44:05 +0800

    添加创建圆角窗口的支持

--- Tools/Form.cs
+++ Tools/Form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Tools
 {
@@ -20,12 +21,32 @@ namespace Tools
             [DllImport("user32.dll")]
             public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
 
+            [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+            public static extern IntPtr CreateRoundRectRgn(
+                int nLeftRect, // x-coordinate of upper-left corner
+                int nTopRect, // y-coordinate of upper-left corner
+                int nRightRect, // x-coordinate of lower-right corner
+                int nBottomRect, // y-coordinate of lower-right corner
+                int nWidthEllipse, // height of ellipse
+                int nHeightEllipse // width of ellipse
+            );
+
+            /// <summary>
+            /// 设置窗口为圆角
+            /// </summary>
+            /// <param name="handle"></param>
+            /// <param name="widthEllipse"></param>
+            /// <param name="heightEllipse"></param>
+            public static void SetFormRound(Form handle, int widthEllipse, int heightEllipse) {
+                handle.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, handle.Width, handle.Height, widthEllipse, heightEllipse));
+            }
+
             /// <summary>
             /// 移动窗口，代码放在Down事件下
             /// </summary>
             /// <param name="handle">窗口句柄[(IntPtr)this.handle]</param>
             /// <returns>消息是否发送成功</returns>
-            public static bool MoveFrom(IntPtr handle)
+            public static bool MoveForm(IntPtr handle)
             {
                 ReleaseCapture();
                 return SendMessage(handle, 0XA1, 2, 0);
